1777967eb7625d97b13aa4f76f2fb94e1558541a
Drodt <daniel.drodt@tu-darmstadt.de>
2023-06-28 15:33:27 +0200

    test multiple specs

--- crates/example/src/main.rs
+++ crates/example/src/main.rs
@@ -7,6 +7,14 @@ fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
+#[spec("b is not zero", requires(b != 0), ensures(result == a / b))]
+#[spec("b is zero", requires(b == 0), panics)]
+fn div(a: u64, b: u64) -> u64 {
+    a / b
+}
+
 pub fn main() {
     println!("{}", add(2, 8));
+    println!("{}", div(8, 4));
+    println!("{}", div(4, 0));
 }
