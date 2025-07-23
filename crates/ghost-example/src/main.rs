9c6b18bbb9ac1353542274b4b3032647d6755710
yseulp <yeseul.park@stud.tu-darmstadt.de>
2025-07-23 10:42:03 +0200

    examples for valid ghost-syntax

--- crates/ghost-example/src/main.rs
+++ crates/ghost-example/src/main.rs
@@ -19,4 +19,39 @@ fn add(a: i32, b: i32) -> i32 {
     a + 1
 }
 
+struct MyStruct {
+    f1: Vec<i32>,
+}
+
+fn allowed() {
+    // Copy type (i32): can be used directly
+    let x: i32 = 5;
+    let y = ghost! {
+        x + 1;
+    };
+
+    //  Reference to non-Copy (String): allowed
+    let s: String = String::from("abc");
+    ghost! {
+        (&s).len()
+    };
+
+    // Clone of non-Copy (String): explicitly allowed
+    let s1: String = String::from("abc");
+    let y1 = ghost! {
+        s.clone().len()
+    };
+
+    //  Clone of Vec (non-Copy): explicitly allowed
+    let vec = vec![1, 2, 3];
+    ghost! {
+        let ghost_vec = vec.clone();
+        ghost_vec.len()
+    };
+
+    // TODO
+    let ms = MyStruct { f1: vec![0] };
+    // let gms: Ghost<MyStruct> = ghost! {snapshot!(ms)};
+}
+
 fn main() {}
