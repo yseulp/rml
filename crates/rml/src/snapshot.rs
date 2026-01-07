d94758d1ccb78dbdfba14bb861ede8f1a433c102
Drodt <daniel.drodt@tu-darmstadt.de>
2026-01-07 13:51:55 +0100

    Simplify if let chain

--- crates/rml/src/snapshot.rs
+++ crates/rml/src/snapshot.rs
@@ -7,23 +7,21 @@ use crate::{
 };
 
 pub(crate) fn get_snapshot_expr(tcx: TyCtxt, expr: &rustc_hir::Expr) -> Option<rustc_hir::BodyId> {
-    if let rustc_hir::ExprKind::Call(path_expr, args) = expr.kind {
-        if let rustc_hir::ExprKind::Path(ref qpath) = path_expr.kind
-            && let rustc_hir::QPath::Resolved(None, p) = qpath
-            && let Some(did) = p.res.opt_def_id()
-        {
-            if tcx.def_path_str(did).contains("snapshot_from_fn") {
-                if let Some(arg) = args.get(0) {
-                    if let rustc_hir::ExprKind::Closure(c) = arg.kind {
-                        let did = c.def_id.to_def_id();
-                        if is_snapshot(tcx, did) {
-                            return Some(c.body);
-                        }
-                    }
-                }
-            }
+    if let rustc_hir::ExprKind::Call(path_expr, args) = expr.kind
+        && let rustc_hir::ExprKind::Path(ref qpath) = path_expr.kind
+        && let rustc_hir::QPath::Resolved(None, p) = qpath
+        && let Some(did) = p.res.opt_def_id()
+        && tcx.def_path_str(did).contains("snapshot_from_fn")
+        && let Some(arg) = args.get(0)
+        && let rustc_hir::ExprKind::Closure(c) = arg.kind
+    {
+        let did = c.def_id.to_def_id();
+
+        if is_snapshot(tcx, did) {
+            return Some(c.body);
         }
     }
+
     None
 }
 
